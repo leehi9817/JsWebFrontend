8f6d4cd949fbb6dbe0a9e4e617e4a838240f597a
leehi9817 <leehi9817@gmail.com>
2024-09-05 15:54:07 +0900

    이미지 갤러리 및 선택 기능 만들기

---
+++ 12/script.js
@@ -0,0 +1,25 @@
+var req = new XMLHttpRequest(); // 객체 생성
+req.open("GET", "./json/image_list.json"); // GET 메소드로 경로의 파일 얻어오게 설정
+req.onreadystatechange = function() { // onreadystatechange에 대한 callback function
+    if (this.readyState == 4 ) { // 모든 데이터가 정상적으로 수신 되었을 때
+        // console.log(this.response); // response 출력
+        var data = JSON.parse(this.response);
+        for (var i = 0; i < data.length; i++) {
+            // div 태그 생성
+            var div = document.createElement("div");
+            // div에 image 클래스 속성 설정
+            div.setAttribute("class", "image");
+            // div에 onclick 이벤트 추가
+            div.onclick = function() {
+                // image-selected 클래스 속성 추가/제거
+                this.classList.toggle("image-selected");
+                
+            }
+            var img = document.createElement("img");
+            img.src = data[i];
+            div.appendChild(img);
+            document.body.appendChild(div);
+        }
+    }
+}
+req.send(); // request 전송
